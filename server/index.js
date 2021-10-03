efa266830f320648f0764d9a409ec536b7303ad9
Tpopescu33 <85653313+Tpopescu33@users.noreply.github.com>
2021-10-03 17:10:10 -0500

    installed mysql

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
-
 const PORT = process.env.PORT || 3001;
-
 const app = express();
+const mysql = require('mysql');
 
+
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: 'password',
+    database: 'resturant-reservation-system'
+})
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
