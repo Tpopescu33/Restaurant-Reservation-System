a35052868b325c05b07fdc066bf20384e3fd84a9
Tpopescu33 <85653313+Tpopescu33@users.noreply.github.com>
2021-10-03 16:03:22 -0500

    added/tested server

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from "react-router-dom";
 import "./Home.css";
 
 function Home() {
 
-    
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+        fetch('/api')
+        .then((res) => res.json())
+        .then((data) => setData(data.message));
+    }, []);
+
 
     return (
         <div>
             
-                <h1 className= 'sub-title1'>**Placeholder** </h1>
+                <h1 className= 'sub-title1'>{data} </h1>
                
             
         </div>
