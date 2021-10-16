26bbd019b353acec8e0e36709eedbfd1022fa520
Tpopescu33 <85653313+Tpopescu33@users.noreply.github.com>
2021-10-15 19:54:50 -0500

    server inserts into DB

--- server/index.js
+++ server/index.js
@@ -20,10 +20,11 @@ app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
 
-//---------------------------RESERVATION----------------------------------------//
+//---------------------------RESERVATION-START---------------------------------------//
 
 
 app.post('/MakeReservation', (req,res) => {
+  const userID = 1
   const fullName = req.body.fullName
   const contactNumber = req.body.contactNumber
   const emailAddress = req.body.emailAddress
@@ -31,9 +32,20 @@ app.post('/MakeReservation', (req,res) => {
   const resDate = req.body.resDate
   const resTime = req.body.resTime
   const table = req.body.table
-
+  const sqlinsert = "INSERT INTO `reservation` (`userID`, `fullName`, `contactNumber`, `emailAddress`, `numGuests`, `resDate`, `resTime`, `table`) VALUES (?,?,?,?,?,?,?,?)"
   console.log(fullName, contactNumber,emailAddress, numGuests, resDate, resTime, table)
 
+  db.query(sqlinsert, 
+    [userID, fullName, contactNumber, emailAddress, numGuests,resDate, resTime, table], 
+    (err, result) => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log("success")
+      res.send("Values inserted")
+    }
+  })
+  
 })
 
 
@@ -43,7 +55,7 @@ app.post('/MakeReservation', (req,res) => {
 
 
 
-//---------------------------RESERVATION----------------------------------------//
+//---------------------------RESERVATION-END---------------------------------------//
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
