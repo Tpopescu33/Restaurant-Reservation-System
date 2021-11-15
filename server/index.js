99301efa4838577c4cc472c80c6e71e6360aeba3
tpopescu33 <tudor.s.popescu@gmail.com>
2021-11-14 18:34:12 -0600

    test

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const app = express();
 const mysql = require("mysql");
 const cors = require("cors");
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
   user: "root",
   host: "localhost",
-  password: "825351",
+  password: "password",
   port: 3306,
   database: "resturant-reservation-DB",
 });
