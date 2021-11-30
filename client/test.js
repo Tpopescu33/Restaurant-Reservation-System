f34916711687b302ea7db356599fcb6a85be7794
tpopescu33 <tudor.s.popescu@gmail.com>
2021-11-30 09:23:31 -0600

    test

---
+++ client/test.js
@@ -0,0 +1,7 @@
+const assert = require('chai').assert
+const Reservation = require('../client/src/Components/Reservation/Reservation')
+
+describe('Reservation test', function(){
+    let res = Reservation;
+    
+})
